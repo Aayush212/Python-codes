054ed2471565837d5420ade9b277e24ba03751f1
Aayush Mishra <89451987+Aayush212@users.noreply.github.com>
2023-03-28 23:21:02 +0530

    Add files via upload

---
+++ 08_practice_strings.py
@@ -0,0 +1,23 @@
+# # WAP program to show user entered name along with text "Good Morning"
+# a = input("Enter your name : ")
+# print("Good Morning ,\n" + a)
+
+
+# WAP to fill the letter template
+# letter = '''Dear <|name|>,
+# You are Selected !
+
+# Date : <|date|>
+# '''
+# name = input("Enter your name : \n")
+# date = input("Enter date : \n")
+# letter = letter.replace("<|name|>",name)
+# letter =letter.replace("<|date|>",date)
+# print (letter)
+# WAP to detect double spaces in a string
+
+
+# WAP to check double spaces in a string
+st = "This is a string with double    spaces"
+doubleSpaces = st.find(" ")
+print (doubleSpaces)
